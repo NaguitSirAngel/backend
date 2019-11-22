9c2baff5d4c52d339f3ea253459d47add2b1b862
Sir Angel Naguit <angelnaguit@icloud.com>
2019-11-22 14:38:58 -0500

    initial commit

---
+++ database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb://admin:admin@cluster0-shard-00-00-0m5lq.mongodb.net:27017,cluster0-shard-00-01-0m5lq.mongodb.net:27017,cluster0-shard-00-02-0m5lq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+  };
